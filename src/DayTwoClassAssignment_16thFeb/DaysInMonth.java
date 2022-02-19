d6fb8a1f8e893c166555b4395bdd909d9869f435
Ankit kansal <kansal920@gmail.com>
2022-02-19 19:20:20 +0530

    Day two 16th Feb 2022 Assignment

---
+++ src/DayTwoClassAssignment_16thFeb/DaysInMonth.java
@@ -0,0 +1,56 @@
+package DayTwoClassAssignment_16thFeb;
+/*
+* You are given an integer A.
+
+You have to tell how many days are there in the month denoted by A in a non-leap year.
+*
+* */
+import java.util.Scanner;
+
+public class DaysInMonth {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please enter a number equivalent to month to find the number of days it contain");
+        int month = userInput.nextInt();
+        switch (month) {
+            case 1:
+                System.out.println("31");
+                break;
+            case 2:
+                System.out.println("28");
+                break;
+            case 3:
+                System.out.println("31");
+                break;
+            case 4:
+                System.out.println("30");
+                break;
+            case 5:
+                System.out.println("31");
+                break;
+            case 6:
+                System.out.println("30");
+                break;
+            case 7:
+                System.out.println("31");
+                break;
+            case 8:
+                System.out.println("31");
+                break;
+            case 9:
+                System.out.println("30");
+                break;
+            case 10:
+                System.out.println("31");
+                break;
+            case 11:
+                System.out.println("30");
+                break;
+            case 12:
+                System.out.println("31");
+                break;
+            default:
+                System.out.println("Wrong input");
+        }
+    }
+}
---
+++ src/DayTwoClassAssignment_16thFeb/PercentageGrade.java
@@ -0,0 +1,51 @@
+package DayTwoClassAssignment_16thFeb;
+
+import java.util.Scanner;
+
+/*
+* Write a program to input from user five numbers(A, B, C, D & E) representing marks of student
+* in 5 subjects out of 100.
+*  You have to calculate percentage and then Grade of each student.
+* If percentage >= 90% : Grade A
+*If percentage >= 80% : Grade B
+*If percentage >= 70% : Grade C
+*If percentage >= 60% : Grade D
+*If percentage >= 40% : Grade E
+* If percentage < 40% : Grade F
+* Explanation :
+* For Student 1, percentage = (50+60+70+80+90)/5 = 70. Hence grade is C.
+* */
+public class PercentageGrade {
+    public static void main(String[] args) {
+        Scanner userInput=new Scanner(System.in);
+        System.out.println("Enter marks of first Subject");
+        int firstMarks=userInput.nextInt();
+        System.out.println("Enter marks of Second Subject");
+        int secondMarks=userInput.nextInt();
+        System.out.println("Enter marks of Third Subject");
+        int thirdMarks=userInput.nextInt();
+        System.out.println("Enter marks of fourth Subject");
+        int fourthMarks=userInput.nextInt();
+        System.out.println("Enter marks of fifth Subject");
+        int fifthMarks=userInput.nextInt();
+        int percentage=(firstMarks+secondMarks+thirdMarks+fourthMarks+fifthMarks)/5;
+        if(percentage>=90){
+            System.out.println(percentage + " A");
+        }
+        else if(percentage>=80){
+            System.out.println(percentage + " B");
+        }
+        else if(percentage>=70){
+            System.out.println(percentage + " C");
+        }
+        else if(percentage>=60){
+            System.out.println(percentage + " D");
+        }
+        else if(percentage>=40){
+            System.out.println(percentage + " E");
+        }
+        else if(percentage<40){
+            System.out.println(percentage + " F");
+        }
+    }
+}
